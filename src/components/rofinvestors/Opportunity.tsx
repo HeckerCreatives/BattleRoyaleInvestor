4ea84e71f245147d03fb1efa4a5a7229f5b6060a
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2025-03-07 13:50:05 +0800

    pooosh

--- src/components/rofinvestors/Opportunity.tsx
+++ src/components/rofinvestors/Opportunity.tsx
@@ -311,10 +311,16 @@ export default function Opportunity() {
                          delay: 0
                      }}
                     className=' w-full flex items-center justify-center mdmt-12'>
-                        <button className=' relative drop-shadow-lg flex items-center justify-center hover:scale-105 duration-300 transition-all'>
-                            <img src="/assets/BUTTON.png" alt="button" width={500} height={500} className=' ~w-72/96'/>
-                            <p className=' ~text-sm/xl font-bold text-white absolute'>DOWNLOAD THE PITCH DECK</p>
-                        </button>
+                        <a 
+                        href="/Rise of Fearless Pitch Deck.pdf" 
+                        download="Rise of Fearless Pitch Deck.pdf" >
+                            <button className=' relative drop-shadow-lg flex items-center justify-center hover:scale-105 duration-300 transition-all'>
+                                <img src="/assets/BUTTON.png" alt="button" width={500} height={500} className=' ~w-72/96'/>
+                                <p className=' ~text-sm/xl font-bold text-white absolute'>DOWNLOAD THE PITCH DECK</p>
+                            </button>
+                        </a>
+
+                        
 
                     </motion.div>
                     
@@ -323,10 +329,6 @@ export default function Opportunity() {
 
             </div>
         </div>
-
-
-
-
     </div>
   )
 }
